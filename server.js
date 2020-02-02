1b21b6b1ea7e94942d8bc882c6f8324467d8b0d5
Karine Liuti <kah.liuti@gmail.com>
2020-02-02 10:36:59 -0300

    Fixed path of routes

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const app = require('./src/config/custom-express');
 
-const routes = require('./src/app/routes/routes');
-routes(app);
-
 app.listen(3000, () => {
   console.log('Server started on port 3000')
 })
